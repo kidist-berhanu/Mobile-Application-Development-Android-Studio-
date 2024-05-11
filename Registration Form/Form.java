b85093a1a55b7171be784462c2b5e9c7ea5d3176
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-05-11 20:54:03 +0300

    Update Form.java

--- Registration Form/Form.java
+++ Registration Form/Form.java
@@ -1,1 +1,21 @@
+package com.example.assignment;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
 
