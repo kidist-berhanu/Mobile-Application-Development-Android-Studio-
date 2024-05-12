e03a6a54227afaab62f61d0f0efe1275f0556777
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-05-12 23:20:44 +0300

    Update Display.java

--- Registration Form/Display.java
+++ Registration Form/Display.java
@@ -1,1 +1,11 @@
+package com.example.assignment;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
