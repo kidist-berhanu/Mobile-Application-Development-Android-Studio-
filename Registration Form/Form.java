21222fd0a1bd75313bcaa5cbfb3b2b39e9fdda1b
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-05-13 19:30:04 +0300

    Update Form.java

--- Registration Form/Form.java
+++ Registration Form/Form.java
@@ -20,56 +20,30 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    public Button button;
+    private static final int imageRequestCode = 1;
+    public ImageView image;
+    public Button imgButton;
+    public Button dateButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.datePickerButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        image = findViewById(R.id.ivImage);
+        imgButton = findViewById(R.id.uploadbtn);
+        imgButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                openDatePicker(); // Open date picker dialog
+                openImagePicker();
             }
         });
 
-    }
- public void openDatePicker(){
-        // current date
-        Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
-
-
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+        dateButton = findViewById(R.id.datePickerButton);
+        dateButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                button.setText(String.valueOf(year) + "." + String.valueOf(month) + "." + String.valueOf(dayOfMonth));
+            public void onClick(View view) {
+                openDatePicker();
             }
-        }, year, month, dayOfMonth);
-
-        datePickerDialog.show();
+        });
     }
-
-    public void onRegisterClicked(View v){
-        Intent intent = new Intent(this,Display.class);
-        startActivity(intent);
-    }
-    
-   @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-       getMenuInflater().inflate(R.menu.option_menu,menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()==R.id.exit){
-            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
-        }
-        return true;
-    }
-
-}
