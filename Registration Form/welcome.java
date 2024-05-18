ff3550190e22dcdd919ef5f3885f68b74a0cef1b
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-05-18 21:32:05 +0300

    Update welcome.java

--- Registration Form/welcome.java
+++ Registration Form/welcome.java
@@ -1,1 +1,131 @@
 
+package com.example.assignment;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
+
+import java.util.Locale;
+
+public class Welcome extends AppCompatActivity {
+    ListView company;
+    private ActionMode actionMode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        company = findViewById(R.id.lvcompany);
+        registerForContextMenu(company);
+
+        company.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                if (actionMode != null) {
+                    return false;
+                }
+                // Start the ActionMode
+                actionMode = startSupportActionMode(actionModeCallback);
+                view.setSelected(true); // Highlight the selected item in the ListView
+                return true;
+            }
+        });
+    }
+
+    private ActionMode.Callback actionModeCallback = new ActionMode.Callback() {
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            getMenuInflater().inflate(R.menu.context_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false; // Return false if nothing is done.
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            if (item.getItemId() == R.id.apply) {
+                Intent intent = new Intent(Welcome.this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            actionMode = null;
+        }
+    };
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.exit) {
+            showExitDialog();
+            return true;
+        }
+        if (id == R.id.amh) {
+            setLocal(this,"am");
+            recreate();
+            return true;
+        }
+        if (id == R.id.eng) {
+            setLocal(this,"en");
+            recreate();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void setLocal(Activity activity, String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+        Resources resources = activity.getResources();
+        Configuration configuration = resources.getConfiguration();
+        configuration.setLocale(locale);
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+        //saveLanguagePreference(languageCode);
+    }
+    private void showExitDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to exit?")
+                .setTitle("Exit")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
