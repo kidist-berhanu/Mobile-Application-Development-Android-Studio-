e3330ffb7b229f2d70febd5552a45bf46a25110f
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-05-18 21:32:49 +0300

    Update Form.java

--- Registration Form/Form.java
+++ Registration Form/Form.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -30,33 +31,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.assignment.databinding.ActivityMainBinding;
+
 import java.util.Calendar;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    //variables  for saveinstance method
     private String name;
     private String email;
     private String phone;
     private String dob;
     private String gender;
     private String educationLevel;
-    private String areaOfInterest;
     private Uri imageUri;
-
     private static final int imageRequestCode = 1;
+    // Variables for storing user inputs
     public ImageView image;
+    public EditText Name;
+    public EditText Email;
+    public EditText Phone;
+    public Spinner spinner ;
+    public String selectedGender;
     public Button imgButton;
     public Button dateButton;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         String selectedLanguage = getLanguagePreference();
-
-        setLocal(this, selectedLanguage);
+        // Set the locale only if it hasn't been set already
+        if (!selectedLanguage.isEmpty()) {
+            setLocal(this, selectedLanguage);
+        }
 
         if (savedInstanceState != null) {
             // Restore saved instance state
@@ -64,32 +76,35 @@ public class MainActivity extends AppCompatActivity {
             email = savedInstanceState.getString("KEY_EMAIL");
             phone = savedInstanceState.getString("KEY_PHONE");
             dob = savedInstanceState.getString("KEY_DOB");
+            //dateButton.setText(dob);
             gender = savedInstanceState.getString("KEY_GENDER");
             educationLevel = savedInstanceState.getString("KEY_EDUCATION_LEVEL");
-            areaOfInterest = savedInstanceState.getString("KEY_AREA_OF_INTEREST");
             imageUri = savedInstanceState.getParcelable("KEY_IMAGE_URI");
 
             if (imageUri != null) {
                 image.setImageURI(imageUri);
             }
         }
-        image = findViewById(R.id.ivImage);
-        imgButton = findViewById(R.id.uploadbtn);
+        image = binding.ivImage;
+        imgButton = binding.uploadbtn;
         imgButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 openImagePicker();
             }
         });
+        dateButton = binding.datePickerButton;
 
-        dateButton = findViewById(R.id.datePickerButton);
         dateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openDatePicker();
-            }
+                      @Override
+                      public void onClick(View v) {
+                          openDatePicker();
+                      }
         });
-
+        Name=binding.etName;
+        Email=binding.etEmail;
+        Phone=binding.etPhone;
+        spinner = binding.spinner;
     }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -101,23 +116,9 @@ public class MainActivity extends AppCompatActivity {
         outState.putString("KEY_DOB", dob);
         outState.putString("KEY_GENDER", gender);
         outState.putString("KEY_EDUCATION_LEVEL", educationLevel);
-        outState.putString("KEY_AREA_OF_INTEREST", areaOfInterest);
         outState.putParcelable("KEY_IMAGE_URI", imageUri);
     }
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        name = savedInstanceState.getString("name");
-        email = savedInstanceState.getString("email");
-        phone = savedInstanceState.getString("phone");
-        dob = savedInstanceState.getString("dob");
-        gender = savedInstanceState.getString("gender");
-        educationLevel = savedInstanceState.getString("educationLevel");
-      //  interests = savedInstanceState.getStringArrayList("interests");
-        imageUri = savedInstanceState.getParcelable("imageUri");
 
-
-    }
     public void openImagePicker() {
         Intent intent = new Intent();
         intent.setType("image/*");
@@ -130,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == imageRequestCode && resultCode == RESULT_OK && data != null && data.getData() != null) {
             Uri imageUri = data.getData();
             image.setImageURI(imageUri);
+            this.imageUri = imageUri;
         }
     }
     public void openDatePicker(){
@@ -139,11 +141,11 @@ public class MainActivity extends AppCompatActivity {
         int month = calendar.get(Calendar.MONTH);
         int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
 
-
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 dateButton.setText(String.valueOf(year) + "." + String.valueOf(month) + "." + String.valueOf(dayOfMonth));
+                dob=dateButton.getText().toString();
             }
         }, year, month, dayOfMonth);
 
@@ -181,7 +183,8 @@ public class MainActivity extends AppCompatActivity {
         Configuration configuration = resources.getConfiguration();
         configuration.setLocale(locale);
         resources.updateConfiguration(configuration, resources.getDisplayMetrics());
-        saveLanguagePreference(languageCode);
+
+        //saveLanguagePreference(languageCode);
     }
     private void saveLanguagePreference(String languageCode) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
@@ -210,52 +213,36 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog alert = builder.create();
         alert.show();
     }
-
-   /* public void onRadioButtonClicked(View view) {
-        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGrp);
-
-       // RadioButton femaleRadioButton = findViewById(R.id.femaleRbtn);
-       // RadioButton maleRadioButton = findViewById(R.id.maleRbtn);
-
+   public void onRadioButtonClicked(View view) {
+        RadioGroup radioGroup = (RadioGroup) binding.radioGrp;
         int id = radioGroup.getCheckedRadioButtonId();
-        switch(id) {
-            case R.id.maleRbtn:
 
-                break;
-            case R.id.femaleRbtn:
+       if (id == R.id.maleRbtn) {
+           selectedGender = "Male";
+       } else if (id == R.id.femaleRbtn) {
+           selectedGender = "Female";
+       } else {
+           selectedGender = " ";
+       }
 
-                break;
-        }
-    }
-    Spinner spinner = (Spinner) findViewById(R.id.spinner);
-    String string = String.valueOf(spinner.getSelectedItem());
+   }
 
-    public void onCheckboxClicked(View view) {
-        // Has the checkbox that was clicked been checked?
-    boolean checked = ((CheckBox) view).isChecked();
 
-    // Retrieve which checkbox was clicked
-        switch(view.getId()) {
-            case R.id.GraphicDesign:
-                if (checked) {}
-                else {}
-                break;
-            case R.id.FED:
-                if (checked) {}
-                else {}
-                break;
-            case R.id.BED:
-                if (checked) {}
-                else {}
-                break;
-            case R.id.AI:
-                if (checked) {}
-                else {}
-                break;
-        }
-    }*/
     public void onRegisterClicked(View v){
-        Intent intent = new Intent(this,Display.class);
+        Intent intent = new Intent(this, Display.class);
+
+        intent.putExtra("IMAGE_URI", imageUri.toString());
+        intent.putExtra("FULL_NAME", Name.getText().toString());
+        intent.putExtra("EMAIL", Email.getText().toString());
+        intent.putExtra("PHONE", Phone.getText().toString());
+        intent.putExtra("DOB", dob);
+        intent.putExtra("GENDER", selectedGender);
+
+        String spinnerValue = spinner.getSelectedItem().toString();
+        intent.putExtra("EDUCATION_LEVEL", spinnerValue);
+
+
         startActivity(intent);
     }
+
 }
