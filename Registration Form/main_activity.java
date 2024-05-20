336372328489b135787b0b0af864ae2bd684d39a
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-05-20 20:10:29 +0300

    Update main_activity.java

--- Registration Form/main_activity.java
+++ Registration Form/main_activity.java
@@ -1,8 +1,8 @@
 package com.example.assignment;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -14,22 +14,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.assignment.databinding.ActivityMainBinding;
 
@@ -37,7 +29,6 @@ import java.util.Calendar;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    //variables  for saveinstance method
     private String name;
     private String email;
     private String phone;
@@ -46,12 +37,12 @@ public class MainActivity extends AppCompatActivity {
     private String educationLevel;
     private Uri imageUri;
     private static final int imageRequestCode = 1;
-    // Variables for storing user inputs
+
     public ImageView image;
     public EditText Name;
     public EditText Email;
     public EditText Phone;
-    public Spinner spinner ;
+    public Spinner spinner;
     public String selectedGender;
     public Button imgButton;
     public Button dateButton;
@@ -64,59 +55,71 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        // Retrieve and apply language preference
         String selectedLanguage = getLanguagePreference();
-        // Set the locale only if it hasn't been set already
         if (!selectedLanguage.isEmpty()) {
-            setLocal(this, selectedLanguage);
+            setLocale(this, selectedLanguage);
         }
 
+        // Restore saved instance state
         if (savedInstanceState != null) {
-            // Restore saved instance state
             name = savedInstanceState.getString("KEY_NAME");
             email = savedInstanceState.getString("KEY_EMAIL");
             phone = savedInstanceState.getString("KEY_PHONE");
             dob = savedInstanceState.getString("KEY_DOB");
-            //dateButton.setText(dob);
             gender = savedInstanceState.getString("KEY_GENDER");
             educationLevel = savedInstanceState.getString("KEY_EDUCATION_LEVEL");
             imageUri = savedInstanceState.getParcelable("KEY_IMAGE_URI");
-
-            if (imageUri != null) {
-                image.setImageURI(imageUri);
-            }
         }
+
+        // Initialize views
         image = binding.ivImage;
         imgButton = binding.uploadbtn;
-        imgButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openImagePicker();
-            }
-        });
+        imgButton.setOnClickListener(v -> openImagePicker());
         dateButton = binding.datePickerButton;
+        dateButton.setOnClickListener(v -> openDatePicker());
 
-        dateButton.setOnClickListener(new View.OnClickListener() {
-                      @Override
-                      public void onClick(View v) {
-                          openDatePicker();
-                      }
-        });
-        Name=binding.etName;
-        Email=binding.etEmail;
-        Phone=binding.etPhone;
+        Name = binding.etName;
+        Email = binding.etEmail;
+        Phone = binding.etPhone;
         spinner = binding.spinner;
+
+        // Set image if available
+        if (imageUri != null) {
+            image.setImageURI(imageUri);
+        }
+
+        binding.registerBtn.setOnClickListener(v -> onRegisterClicked());
     }
+
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         // Save instance state
-        outState.putString("KEY_NAME", name);
-        outState.putString("KEY_EMAIL", email);
-        outState.putString("KEY_PHONE", phone);
+        outState.putString("KEY_NAME", Name.getText().toString());
+        outState.putString("KEY_EMAIL", Email.getText().toString());
+        outState.putString("KEY_PHONE", Phone.getText().toString());
         outState.putString("KEY_DOB", dob);
-        outState.putString("KEY_GENDER", gender);
-        outState.putString("KEY_EDUCATION_LEVEL", educationLevel);
+        outState.putString("KEY_GENDER", selectedGender);
+        outState.putString("KEY_EDUCATION_LEVEL", spinner.getSelectedItem().toString());
         outState.putParcelable("KEY_IMAGE_URI", imageUri);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore instance state
+        Name.setText(savedInstanceState.getString("KEY_NAME"));
+        Email.setText(savedInstanceState.getString("KEY_EMAIL"));
+        Phone.setText(savedInstanceState.getString("KEY_PHONE"));
+        dob = savedInstanceState.getString("KEY_DOB");
+        dateButton.setText(dob);
+        selectedGender = savedInstanceState.getString("KEY_GENDER");
+        educationLevel = savedInstanceState.getString("KEY_EDUCATION_LEVEL");
+        imageUri = savedInstanceState.getParcelable("KEY_IMAGE_URI");
+        if (imageUri != null) {
+            image.setImageURI(imageUri);
+        }
     }
 
     public void openImagePicker() {
@@ -125,67 +128,66 @@ public class MainActivity extends AppCompatActivity {
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), imageRequestCode);
     }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if (requestCode == imageRequestCode && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri imageUri = data.getData();
+            imageUri = data.getData();
             image.setImageURI(imageUri);
-            this.imageUri = imageUri;
         }
     }
-    public void openDatePicker(){
-        // current date
+
+    public void openDatePicker() {
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
         int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
 
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                dateButton.setText(String.valueOf(year) + "." + String.valueOf(month) + "." + String.valueOf(dayOfMonth));
-                dob=dateButton.getText().toString();
-            }
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view, Year, Month, DayOfMonth) -> {
+            dateButton.setText(Year + "." + (Month + 1) + "." + DayOfMonth);
+            dob = dateButton.getText().toString();
         }, year, month, dayOfMonth);
 
         datePickerDialog.show();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-       getMenuInflater().inflate(R.menu.option_menu,menu);
+        getMenuInflater().inflate(R.menu.option_menu, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
         if (id == R.id.exit) {
             showExitDialog();
             return true;
         }
         if (id == R.id.amh) {
-            setLocal(this,"am");
+            setLocale(this, "am");
             recreate();
             return true;
         }
         if (id == R.id.eng) {
-            setLocal(this,"en");
+            setLocale(this, "en");
             recreate();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
-    private void setLocal(Activity activity, String languageCode) {
+
+    private void setLocale(Activity activity, String languageCode) {
         Locale locale = new Locale(languageCode);
         Locale.setDefault(locale);
         Resources resources = activity.getResources();
         Configuration configuration = resources.getConfiguration();
         configuration.setLocale(locale);
         resources.updateConfiguration(configuration, resources.getDisplayMetrics());
-
-        //saveLanguagePreference(languageCode);
+        saveLanguagePreference(languageCode);
     }
+
     private void saveLanguagePreference(String languageCode) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         preferences.edit().putString("language", languageCode).apply();
@@ -193,56 +195,48 @@ public class MainActivity extends AppCompatActivity {
 
     private String getLanguagePreference() {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        return preferences.getString("language", "en"); // Default to English if preference not found
+        return preferences.getString("language", "en");
     }
+
     private void showExitDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Are you sure you want to exit?")
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to exit?")
                 .setTitle("Exit")
                 .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        finish();
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
-        AlertDialog alert = builder.create();
-        alert.show();
+                .setPositiveButton("Yes", (dialog, id) -> finish())
+                .setNegativeButton("No", (dialog, id) -> dialog.cancel())
+                .create()
+                .show();
     }
-   public void onRadioButtonClicked(View view) {
+    public void onRadioButtonClicked(View view) {
         RadioGroup radioGroup = (RadioGroup) binding.radioGrp;
         int id = radioGroup.getCheckedRadioButtonId();
 
-       if (id == R.id.maleRbtn) {
-           selectedGender = "Male";
-       } else if (id == R.id.femaleRbtn) {
-           selectedGender = "Female";
-       } else {
-           selectedGender = " ";
-       }
+        if (id == R.id.maleRbtn) {
+            selectedGender = "Male";
+        } else if (id == R.id.femaleRbtn) {
+            selectedGender = "Female";
+        } else {
+            selectedGender = " ";
+        }
+    }
 
-   }
+    public void onRegisterClicked() {
 
+        if (imageUri == null) {
+            Toast.makeText(this, "Please select an image", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-    public void onRegisterClicked(View v){
         Intent intent = new Intent(this, Display.class);
-
         intent.putExtra("IMAGE_URI", imageUri.toString());
         intent.putExtra("FULL_NAME", Name.getText().toString());
         intent.putExtra("EMAIL", Email.getText().toString());
         intent.putExtra("PHONE", Phone.getText().toString());
         intent.putExtra("DOB", dob);
         intent.putExtra("GENDER", selectedGender);
-
-        String spinnerValue = spinner.getSelectedItem().toString();
-        intent.putExtra("EDUCATION_LEVEL", spinnerValue);
-
+        intent.putExtra("EDUCATION_LEVEL", spinner.getSelectedItem().toString());
 
         startActivity(intent);
     }
-
 }
